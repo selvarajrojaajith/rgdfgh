50751629ce5fb22aac927e17f00adccf35b704f0
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-03-21 16:51:48 +0530

    sum.c
    good

---
+++ sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+void main()
+{
+    long num, digit, sum = 0;
+ 
+    printf("Enter the number \n");
+    scanf("%ld", &num);
+    while (num > 0)
+    {
+        digit = num % 10;
+        sum  = sum + digit;
+        num /= 10;
+    }
+    printf("Given number = %ld\n", temp);
+    printf("Sum of the digits %ld = %ld\n",sum);
+}
